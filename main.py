cee6e94c6f74617d4fe64a2f9443048fc2caec6b
ifgeniy <jon_vy@mail.ru>
2021-05-11 20:18:32 +0300

    Делаю скрин. Код очищен от лишнего.

--- main.py
+++ main.py
@@ -7,17 +7,9 @@ def on_click(x, y, button, pressed):
     if pressed:
         s.append(x)
         s.append(y)
-        x_1 = x
-        y_1 = y
-        pressed_status = 'on'
-        print(f"Позиция курсора: x: {x_1}, y: {y_1} | Статус нажатия: {pressed_status} | Кнопка: {button}")
     else:
         s.append(x)
         s.append(y)
-        x_2 = x
-        y_2 = y
-        pressed_status = 'off'
-        print(f"Позиция курсора: x: {x_2}, y: {y_2} | Статус нажатия: {pressed_status} | Кнопка: {button}")
         return False
 
 
@@ -28,13 +20,10 @@ with mouse.Listener(on_click=on_click) as listener:
 # Запускаем метод для отслеживания мыши
 listener = mouse.Listener(on_click=on_click)
 listener.start()
+
 x_1 = s[0]
 y_1 = s[1]
-
 x_2 = s[2]
 y_2 = s[3]
-
-print(f"Позиция курсора 1: x: {x_1}, y: {y_1}")
-print(f"Позиция курсора 2: x: {x_2}, y: {y_2}")
 
 screenshot = pyautogui.screenshot('screenshot.png', region=(x_1, y_1, x_2, y_2))  # Скрин области
