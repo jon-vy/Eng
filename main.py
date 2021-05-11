698526b8b82b0c188fd5b502c713153f1bbeb607
ifgeniy <jon_vy@mail.ru>
2021-05-11 20:15:29 +0300

    Делаю скрин

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-import requests
 import pyautogui
 from pynput import mouse
 
-
-# координаты курсора
-def on_move(x, y):
-    print(f"Позиция курсора: x: {x}, y: {y}")
-
-
+s = []  # Список для координат мыши
 # Обработка нажатий
 def on_click(x, y, button, pressed):
-    pressed_status = 'on' if pressed else 'off'
-    print(f"Позиция курсора: x: {x}, y: {y} | Статус нажатия: {pressed_status} | Кнопка: {button}")
-
-
-# def on_click(x, y, button, pressed):
-#     #pressed_status = 'on' if pressed else 'off'
-#     if pressed:
-#         x_1 = x
-#         y_1 = y
-#         pressed_status = 'on'
-#         print(f"Позиция курсора: x: {x_1}, y: {y_1} | Статус нажатия: {pressed_status} | Кнопка: {button}")
-#     else:
-#         x_2 = x
-#         y_2 = y
-#         pressed_status = 'off'
-#         print(f"Позиция курсора: x: {x_2}, y: {y_2} | Статус нажатия: {pressed_status} | Кнопка: {button}")
-#         return False
+    if pressed:
+        s.append(x)
+        s.append(y)
+        x_1 = x
+        y_1 = y
+        pressed_status = 'on'
+        print(f"Позиция курсора: x: {x_1}, y: {y_1} | Статус нажатия: {pressed_status} | Кнопка: {button}")
+    else:
+        s.append(x)
+        s.append(y)
+        x_2 = x
+        y_2 = y
+        pressed_status = 'off'
+        print(f"Позиция курсора: x: {x_2}, y: {y_2} | Статус нажатия: {pressed_status} | Кнопка: {button}")
+        return False
 
 
 # Собираем события пока не закончится поток
-with mouse.Listener(
-        # on_move=on_move,
-        on_click=on_click) as listener:
+with mouse.Listener(on_click=on_click) as listener:
     listener.join()
 
 # Запускаем метод для отслеживания мыши
-listener = mouse.Listener(
-    # on_move=on_move,
-    on_click=on_click)
+listener = mouse.Listener(on_click=on_click)
 listener.start()
+x_1 = s[0]
+y_1 = s[1]
 
-# print(f"Позиция курсора 1: x: {x_1}, y: {y_1}")
-# print(f"Позиция курсора 2: x: {x_2}, y: {y_2}")
+x_2 = s[2]
+y_2 = s[3]
 
-# screen = pyautogui.screenshot('s.png')  # Скрин всего экрана
-# screenshot = pyautogui.screenshot('screenshot.png', region=(0, 0, 300, 400))  # Скрин области
-# print(screen)
+print(f"Позиция курсора 1: x: {x_1}, y: {y_1}")
+print(f"Позиция курсора 2: x: {x_2}, y: {y_2}")
+
+screenshot = pyautogui.screenshot('screenshot.png', region=(x_1, y_1, x_2, y_2))  # Скрин области
