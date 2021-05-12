0278bc24623b0822a985daa65cc1c5e601fe3189
ifgeniy <jon_vy@mail.ru>
2021-05-12 22:48:45 +0300

    Прозрачность win0 0.01 win1 всегда сверху

--- area.py
+++ area.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 from tkinter import RIGHT, N
+
+from PIL import ImageTk
+from PIL.Image import Image
+
+
 win1 = tk.Tk()  # Создал окно
 win0 = tk.Tk()  # Создал окно
-
 win0.attributes('-fullscreen', True)  # Полноэкранный режим окна
+win0.attributes("-alpha", 0.01)
+win1.attributes("-topmost", True)
+win1.title('Толмач')
+
 win1.geometry('300x300+100+200')
 
 
-win0.config(bg='#fa23e4')
+#win0.configure(background = image1)
 win1.mainloop()
 win0.mainloop()
