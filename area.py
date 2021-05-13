0c4dff9a5e0ea2c683ec04324f103411d5cf3671
ifgeniy <jon_vy@mail.ru>
2021-05-13 11:50:00 +0300

    Прозрачность win0 0.01 win1 всегда сверху Кнопка перевести запускает screen() Кнопка закрыть закрывает оба окна

--- area.py
+++ area.py
@@ -8,11 +8,27 @@ from PIL.Image import Image
 win1 = tk.Tk()  # Создал окно
 win0 = tk.Tk()  # Создал окно
 win0.attributes('-fullscreen', True)  # Полноэкранный режим окна
-win0.attributes("-alpha", 0.01)
-win1.attributes("-topmost", True)
+win0.attributes("-alpha", 0.01)  # прозрачность
+win1.attributes("-topmost", True)  # Всегда по верх других окон
 win1.title('Толмач')
 
+
 win1.geometry('300x300+100+200')
+
+def close():  # Закрывает оба окна
+    win0.destroy()
+    win1.destroy()
+
+
+
+close = tk.Button(win1, text='Закрыть', command=close)
+close.pack(side=RIGHT, anchor=N)
+
+def screen():
+    print('Скрин')
+
+translit = tk.Button(win1, text='Перевести', command=screen)
+translit.pack(side=RIGHT, anchor=N)
 
 
 #win0.configure(background = image1)
