a5209aef9c10369ecc696e7fe0583a6f0ce5ae4f
ifgeniy <jon_vy@mail.ru>
2021-05-16 06:30:50 +0300

    При нажатии на print_screen срабатывает screen() из screen.py

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-import pyautogui
-from pynput import mouse
+from pynput import keyboard
+import test
+import screen
 
-s = []  # Список для координат мыши
-# Обработка нажатий
-def on_click(x, y, button, pressed):
-    if pressed:
-        s.append(x)
-        s.append(y)
-    else:
-        s.append(x)
-        s.append(y)
-        return False
+def on_press(key):
+    try:
+        print(f"Клавиша: {key.char}")
+    except AttributeError:
+        print(f"Спец. клавиша: {key}")
 
 
-# Собираем события пока не закончится поток
-with mouse.Listener(on_click=on_click) as listener:
+def on_release(key):
+    if key == keyboard.Key.print_screen:
+        screen.screen()
+        return False  # Остановить слушатель
+
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
     listener.join()
 
-# Запускаем метод для отслеживания мыши
-listener = mouse.Listener(on_click=on_click)
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
 listener.start()
 
-# Координаты скрина
-if (s[2] > s[0]):
-    left = s[0]
-else:
-    left = s[2]
-if (s[3] > s[1]):
-    top = s[1]
-else:
-    top = s[3]
-width = s[2] - s[0]
-height = s[3] - s[1]
-
-# Скрин области
-screenshot = pyautogui.screenshot('screenshot.png', region=(left, top, abs(width), abs(height)))
