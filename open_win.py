46b5688aecf1632d69a7e2835cb4d15e710a2e76
ifgeniy <jon_vy@mail.ru>
2021-05-19 20:29:33 +0300

    Окно на весь экран. Фон картинка. По верх всех окон. Есть нажатие на print_screen (оно же Sys_Req)

--- open_win.py
+++ open_win.py
@@ -3,32 +3,34 @@ from pynput import keyboard
 from tkinter import RIGHT, N
 import pyautogui
 from PIL import ImageTk, Image
+import screen_zone
 
 
 def window():
     pyautogui.screenshot('img/screenshot.png')  # Делает скрин всего экрана
     path = 'img/screenshot.png'
-    win0 = tk.Tk()  # Создал окно
-    win0.attributes('-fullscreen', True)  # Полноэкранный режим окна
+    win = tk.Tk()  # Создал окно
+    win.attributes('-fullscreen', True)  # Полноэкранный режим окна
     #win0.geometry("500x600+300+200")
 
-    win0.attributes("-topmost", True)  # Всегда по верх других окон
-    #win0.after_idle(win0.attributes, '-topmost', False)
+    win.attributes("-topmost", True)  # Всегда по верх других окон
+    #win.after_idle(win0.attributes, '-topmost', False)
     image = Image.open(path)
     width = pyautogui.size()[0]
     ratio = (width / float(image.size[0]))
     height = int((float(image.size[1]) * float(ratio)))
     image = image.resize((width, height), Image.ANTIALIAS)
     image = ImageTk.PhotoImage(image)
-    canvas = tk.Canvas(win0, width=width, height=height)
+    canvas = tk.Canvas(win, width=width, height=height)
     canvas.pack(side="top", fill="both", expand="no")
     canvas.create_image(0, 0, anchor="nw", image=image)
 
-    win0.bind("<Escape>", lambda event: win0.destroy())
+    win.bind("<Escape>", lambda event: win.destroy())
+    win.bind("<KeyPress-Sys_Req>", screen_zone.screen())  # Должна запускать creen_zone.screen()
 
 
 
-    win0.mainloop()
+    win.mainloop()
 
 
-window()
+#window()
